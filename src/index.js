c62aecc8e1ae316cf9ff34c2a3408b7f8c325d9d
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2017-06-06 22:51:49 -0300

    added the initial structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './stylus/main.styl'
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+const [head, ...tail] = arr
+
+console.log('primeiro item do array', head)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          'presets': ['es2015', 'stage-0']
+        },
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/template.html'
+    })
+  ]
+}
